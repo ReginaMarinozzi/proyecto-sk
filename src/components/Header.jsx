f2c2f90335abbcb4ea42c3eae870bec6b3ea73fe
ReginaMarinozzi <regina.marinozzi@gmail.com>
2023-07-06 11:33:45 -0300

    arreglo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
               color="#212121"
               fontWeight={600}
             >
-              Higienesegurida laboral. Salud ocupacional y medio ambiente
+              Higiene y seguridad laboral. Salud ocupacional y medio ambiente
             </Typography>
             <Typography
               sx={{
